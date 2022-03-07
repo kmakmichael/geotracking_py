3af22e43e1dfc28ae20a48b9f2fbf6fa784d12bf
kmakmichael <m_kmak@u.pacific.edu>
2022-03-07 09:50:03 -0800

    quick untested coord grabbing

--- trip.py
+++ trip.py
@@ -1,18 +1,23 @@
 import math
 import nodemap
+import gps
 
 
 tol = 8e-6  # should probably be on a node-to-node basis via road width?
 
 
 def distance(x, y):
-    return 0  # math.sqrt(x*x, y*y)
+    return math.sqrt(x*x + y*y)
 
 
 def navigate(m, route):
     print(f'navigating route:\n{route}')
+    record = [(), ()]
     for target in m:
         print(f'heading towards "{target}"')
-        pos = (0, 0)  # pull this from the GPS chip
+        pos = (0, 0)
         while distance(pos, nodemap.node_pos(m, target)) > tol:
+            pos = gps.get_coords()
+            record[0].append(pos[0])
+            record[1].append(pos[1])
             print(f'too far')
