5f4030712d3790096ac021fe98a964b75c60ce84
kmakmichael <m_kmak@u.pacific.edu>
2022-05-01 19:30:33 -0700

    figure out some threading shared memory garbage

--- macronav.py
+++ macronav.py
@@ -1,8 +1,10 @@
 import networkx
+from math import sqrt
 import nodemap
 #import trip
 import gps
-from threading import Thread, Timer, Event, Lock
+import time
+from threading import Thread, Event, Lock
 from enum import IntEnum
 from btcomms import BTComms
 
@@ -13,48 +15,56 @@ class GPSStatus(IntEnum):
     TURN = 2
 status_lock = Lock()
 status_changed = Event()
-current_status = 0
-
+com_lock = Lock()
+shmem = {
+        "status": 0
+        }
+tol = 8e-5
 
 
 def distance(a,b):
-    return math.sqrt(abs(b[0]-a[0])**2 + abs(b[1]-a[1])**2)
+    return sqrt(abs(b[0]-a[0])**2 + abs(b[1]-a[1])**2)
 
 
-def navigate(m, route, s):
+def navigate(m, route, com):
     print(f'[navigation] navigating from {route[0]} to {route[-1]}')
     # record = ([], [])
+    print('[navigation] setting status to GO')
+    with status_lock and com_lock:
+        shmem["status"] = 1 # int(GPSStatus.GO)
+        com.send('G')
+    status_changed.set()
     pos = gps.get_coords()
     for target in route:
         # print(f'[navigation] heading towards {target}')
-        while distance(pos, nodemap.nose_pos(m, target)) > tol:
+        while distance(pos, nodemap.node_pos(m, target)) > tol:
             pos = gps.get_coords()
             #record[0].append(pos[0])
             #record[1].append(pos[1])
-    with status_lock:
-        s = GPSStatus.STOP
+    with status_lock and com_lock:
+        shmem["status"] = GPSStatus.STOP
+        com.send('S')
     status_changed.set()
     # return record
 
 def status_update(com, s):
-    print(f'[navigation] sending update: {s}')
-    with status_lock:
-        if s == 1:
-            com.send('G')
-        else:
-            com.send('S')
-    ouroboros = Timer(2, status_update, args=[com, s])
-    ouroboros.start()
-
+    while True:
+        time.sleep(2)
+        print(f'[navigation] sending update: {s["status"]}')
+        with status_lock and com_lock:
+            if s["status"] == 1:
+                com.send('G')
+            else:
+                com.send('S')
 
 
 if __name__ == '__main__':
-    print('loading nodemap...')
+    print('[navigation] loading nodemap...')
     # load the node map
     campus_map = networkx.Graph()
     nodemap.load_nodes(campus_map, "mapdata/nodes.csv")
     nodemap.load_edges(campus_map, "mapdata/edges.csv")
-    print('successfully loaded map')
+    print('[navigation] successfully loaded map')
 
     com = BTComms('N')
 
@@ -67,16 +77,16 @@ if __name__ == '__main__':
     print(f'[navigation] fix found')
     com.confirm()
     # start update timer
-    status_update(com, current_status)
+    update = Thread(target=status_update, args=[com, shmem])
+    update.start()
     # start navigating
     #   in actuality, the first thing we need to do is find where we are and then get to point A
     #   so we'd need a function to do that, and then trip.navigate() a path here to A
-    trip = Thread(target=navigate, args=[campus_map, route, current_status])
-    trip.start()
+    navigate(campus_map, route, com)
     while True:
         status_changed.wait()
-        with status_lock:
-            if current_status == 1:
+        with status_lock and com_lock:
+            if shmem["status"] == 1:
                 com.send('G')
             else:
                 com.send('S')
